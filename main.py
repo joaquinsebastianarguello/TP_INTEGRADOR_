62622556620c6ab1b6162b1c036e77aeb5daa8ec
Joaquin Arguello <joaquinsebastian.arguello@gmail.com>
2026-09-21 19:35:12 -0300

    cambio de menu y metodos de busqueda para el tp 02

--- main.py
+++ main.py
@@ -8,11 +8,46 @@ import json
 with open('datos.json', 'r', encoding='utf-8') as datos:
     datos = json.load(datos)
 
+#SEPARADOR
+# ubico aca el arbol, dentro de las opciones no funciona
+class nodo_arbol:   #clase nodo arbol
+    def __init__ (self, cancion):  #crea un nodo con la informacion cargada
+        self.cancion = cancion
+        self.izquierda = None
+        self.derecha = None
+
+def arbolVacio(raiz):
+    return raiz == None
+                 
+def insertarNodo(raiz,cancion): #inserto el dato en el arbol
+    if arbolVacio(raiz):
+        raiz = nodo_arbol(cancion)
+    elif raiz.cancion._titulo.lower() <= cancion._titulo.lower():
+        raiz.derecha = insertarNodo(raiz.derecha, cancion)
+    else:
+        raiz.izquierda = insertarNodo(raiz.izquierda, cancion)
+    return(raiz)
+
+def buscarNodo(raiz, tituloBuscado):
+    if arbolVacio(raiz):
+        return None    
+    titulo = raiz.cancion._titulo.lower().strip()
+    busqueda = tituloBuscado.lower().strip()
+    if busqueda == titulo:
+        return raiz.cancion
+    elif busqueda < titulo:
+        return buscarNodo(raiz.izquierda, tituloBuscado)
+    else:
+        return buscarNodo(raiz.derecha, tituloBuscado)
+
+# SEPARADOR
+
+
 usuario = Usuario("Luis")
 bibliotecaCanciones = [] #home- un generico para mostrar todo
 for i in datos:
     cancion_A = Cancion(i["titulo"], i["artista"], i["genero"], i["letra"], i["calificacion"])
-    bibliotecaCanciones.append(cancion_A)
+    bibliotecaCanciones.append(cancion_A) #NO TOCAR!!!!!!!!!
 #paso la explicacion pq me costo una banda, pero como ya tenemos listas reservadas, añado el generico
 #una vez tengo el generico, hago que "i" buscque cada valor en datos
 #aparentemente, hay que hacer una transferencia para que el formato diccionario de json sea el valor de cancion.py
@@ -20,32 +55,94 @@ for i in datos:
 #y lo añade en la carpeta "bibliotecaCanciones" hasta quedar vacio (o lleno, depende como lo veas)
 #anduve viendo trabajos y probando fuerza bruta hasta que salio, siendo sincero
 
+raizArbol = None
+for cancion in bibliotecaCanciones:
+    raizArbol = insertarNodo (raizArbol, cancion) 
+#recordatorio, a futuro cambiar los valores de busqueda por palabras que den a entender el codigo, en lugar de solo una letra
+
 while True:
 # Mostramos el menú de opciones
     print("\n=== DISCOTECA ===")
-    print("1. BUSCADOR")
-    print("2. BIBLIOTECA")
-    print("3. CREAR PLAYLIST")
-    print("4. FAVORITOS")
-    print("5. EXPLORAR CANCIONES")
+    print("1. BUSCADOR") #mostrar todo, acceder a informacion de una cancion en especifico
+    print("2. BIBLIOTECA")#mostrar playlists, buscar entre ellas, abrirlas, agregar/eliminar cancion en playlist
+    print("3. CREAR PLAYLIST")#crea una playlist vacia y la agrega a biblioteca
+    print("4. FAVORITOS")#playlist default,muestra las canciones que se hayan en favoritos, buscar, agregar
+    print("5. EXPLORAR CANCIONES")#muestra todo, filtrar por genero/rating
     print("6. SALIR")
 
     opcion = input("INGRESE UNA OPCION: ")
 
     if opcion == "1":
+        print("PARA SALIR DE ESTE MENU, INGRESE 'SALIR'")
+        #METODOS TEMPORALES; BORRAR DESPUES
+        print("1. BUSQUEDA SECUENCIAL")
+        print("2. BUSQUEDA DE ARBOL")
+        tipobusqueda = input ("INGRESE EL TIPO DE BUSQUEDA A REALIZAR").lower()
+        #METODOS TEMPORALES; BORRAR DESPUES
         buscar = input("INGRESE EL NOMBRE DE LA CANCION: ").lower()
-        encontrado = False
-        for i in bibliotecaCanciones:
-            if buscar in i._titulo.lower().strip():
-                encontrado = True
-                print (i)
-            else:
+        #METODO TEMPORAL; BORRAR DESPUES
+        if tipobusqueda == "1":
+            encontrado = False
+            for i in bibliotecaCanciones:
+                if buscar in i._titulo.lower().strip():
+                    encontrado = True
+                    print (i)
+            if not encontrado:
                 print("NO SE ENCONTRO NINGUNA CANCION.")
 
+        elif tipobusqueda == "2": #todo aca esta sacado de los ejemplos de arboles_binarios_recorridos/tad_arbol.py en el campus (no recuerdo bien la clase)
+        #nota aparte- parece que el arbol debe ir por fuera de la opcion
+            resultado = buscarNodo(raizArbol, buscar)
+            if resultado != None:
+                print(resultado)
+            else:
+                print("NO SE ENCONTRO NINGUNA CANCION")
+	
+
+    elif opcion == "2":
+        biblioteca = usuario._biblioteca
+        if len(biblioteca) == 0:
+            print("NO HAY ELEMENTOS EN LA BIBLIOTECA")
+        else:
+            for i in biblioteca:
+                print (i)#muestra
+                print("="*30)
+                print("1. BUSCADOR")
+                print("2. EXAMINAR PLAYLIST")
+                print("3. AGREGAR CANCION A UNA PLAYLIST")
+                print("4. ELIMINAR CANCION DE UNA PLAYLIST")
+                print("5. SALIR")
+
+    elif opcion == "3":
+        print("PARA SALIR DE ESTE MENU, INGRESE 'SALIR'")
+        crear = input("INGRESE UN NOMBRE PARA LA NUEVA PLAYLIST: ")
+        if crear == "1":
+            break
+        else:
+            usuario.crearPlaylist(crear)
+            print("PLAYLIST AÑADIDA")
+
+    elif opcion == "4":
+        favoritos = usuario._favoritos
+        if len(biblioteca) == 0:
+            print("NO HAY ELEMENTOS EN LA BIBLIOTECA")
+        else:
+            for i in favoritos:
+                print (i)#muestra
+                print("="*30)
+                print("1. BUSCADOR")
+                print("2. AGREGAR CANCION A UNA PLAYLIST")
+                print("3. ELIMINAR CANCION DE UNA PLAYLIST")
+                print("4. SALIR")
+        
 
     elif opcion == "5":
-        for cancion in datos:
-            print (cancion_A)
+        for cancion in bibliotecaCanciones:
+            print (cancion)
+        print("="*30)
+        print("1.FILTRAR POR GENERO")
+        print("2. FILTRAR POR CALIFICACION")
+        print("3. SALIR")
 
     elif opcion == "6":
         print("Saliendo del programa...")
